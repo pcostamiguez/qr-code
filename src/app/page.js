9b7ffc364ea7c28c45c3db9e5de5c1eaf291ffca
Pedro Henrique Costa Miguez <pcostamiguez@gmail.com>
2025-07-16 19:55:38 -0300

    Ajusta os cantos

--- src/app/page.js
+++ src/app/page.js
@@ -3,9 +3,9 @@ import Image from "next/image";
 export default function Home() {
   return (
     <main className="container flex flex-col items-center">
-      <div className="p-4 w-[320px] h-[497px] bg-white rounded-xl shadow-lg">
-        <div className="flex justify-center bg-blue-500 w-[288px] h-[288px] rounded-md">
-          <Image className="rounded-md" src="/image-qr-code.png" alt="QR Code" width={288} height={288} />
+      <div className="p-4 w-[320px] h-[497px] bg-white rounded-[20px] shadow-lg">
+        <div className="flex justify-center bg-blue-500 w-[288px] h-[288px] rounded-xl">
+          <Image className="rounded-xl" src="/image-qr-code.png" alt="QR Code" width={288} height={288} />
         </div>
         <div className="flex flex-col items-center m-4">
           <h1 className="heading">
